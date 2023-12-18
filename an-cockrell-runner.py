099a4006a1916dd37c75caef8bc59583ac511d24
Adam Knapp <adam.knapp@instanton.org>
2023-12-18 14:01:05 -0500

    save arrays in compressed format

--- an-cockrell-runner.py
+++ an-cockrell-runner.py
@@ -216,39 +216,192 @@ system_health = np.full((num_sims, num_steps), -1, dtype=np.float64)
 
 with h5py.File("run-statistics.hdf5", "w") as f:
     f.create_dataset(
-        "param_list", (num_sims, len(variational_params)), dtype=np.float64, data=param_list
+        "param_list",
+        (num_sims, len(variational_params)),
+        dtype=np.float64,
+        data=param_list,
+        chunks=(100, len(variational_params)),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
     )
-    f.create_dataset("total_T1IFN", (num_sims, num_steps), dtype=np.float64, data=total_T1IFN)
-    f.create_dataset("total_TNF", (num_sims, num_steps), dtype=np.float64, data=total_TNF)
-    f.create_dataset("total_IFNg", (num_sims, num_steps), dtype=np.float64, data=total_IFNg)
-    f.create_dataset("total_IL6", (num_sims, num_steps), dtype=np.float64, data=total_IL6)
-    f.create_dataset("total_IL1", (num_sims, num_steps), dtype=np.float64, data=total_IL1)
-    f.create_dataset("total_IL8", (num_sims, num_steps), dtype=np.float64, data=total_IL8)
-    f.create_dataset("total_IL10", (num_sims, num_steps), dtype=np.float64, data=total_IL10)
-    f.create_dataset("total_IL12", (num_sims, num_steps), dtype=np.float64, data=total_IL12)
-    f.create_dataset("total_IL18", (num_sims, num_steps), dtype=np.float64, data=total_IL18)
+    f.create_dataset(
+        "total_T1IFN",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=total_T1IFN,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "total_TNF",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=total_TNF,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "total_IFNg",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=total_IFNg,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "total_IL6",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=total_IL6,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "total_IL1",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=total_IL1,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "total_IL8",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=total_IL8,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "total_IL10",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=total_IL10,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "total_IL12",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=total_IL12,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "total_IL18",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=total_IL18,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
     f.create_dataset(
         "total_extracellular_virus",
         (num_sims, num_steps),
         dtype=np.float64,
         data=total_extracellular_virus,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
     )
     f.create_dataset(
         "total_intracellular_virus",
         (num_sims, num_steps),
         dtype=np.float64,
         data=total_intracellular_virus,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
     )
     f.create_dataset(
         "apoptosis_eaten_counter",
         (num_sims, num_steps),
         dtype=np.float64,
         data=apoptosis_eaten_counter,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
     )
-    f.create_dataset("infected_epis", (num_sims, num_steps), dtype=np.float64, data=infected_epis)
-    f.create_dataset("dead_epis", (num_sims, num_steps), dtype=np.float64, data=dead_epis)
-    f.create_dataset("apoptosed_epis", (num_sims, num_steps), dtype=np.float64, data=apoptosed_epis)
-    f.create_dataset("system_health", (num_sims, num_steps), dtype=np.float64, data=system_health)
+    f.create_dataset(
+        "infected_epis",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=infected_epis,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "dead_epis",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=dead_epis,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "apoptosed_epis",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=apoptosed_epis,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
+    f.create_dataset(
+        "system_health",
+        (num_sims, num_steps),
+        dtype=np.float64,
+        data=system_health,
+        chunks=(100, num_steps),
+        compression="gzip",
+        compression_opts=9,
+        shuffle=True,
+        fletcher32=True,
+    )
 
 
 lhc = LatinHypercube(len(variational_params))
